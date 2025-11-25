a347775b232df623686b33d0c492a201dc8fe7a8
zlatevv <cvetanzlatev30@gmail.com>
2025-11-25 16:14:02 +0200

    feat. author class

--- CPP/homework_1/Author.cpp
+++ CPP/homework_1/Author.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdexcept>
 using namespace std;
 
 class Author {
@@ -12,22 +13,22 @@ public:
     ~Author() = default;
 
     std::string to_string() const {
-        return "The man, the myth, the legend: " + name + "! Born in " + std::string(birthYear)
+        return "The man, the myth, the legend: " + name + "! Born in " + std::to_string(birthYear);
     };
 
     std::string getName(){return name;}
     int getYear(){return birthYear;}
 
     void setName(std::string name){
-        if (name == null || name.length = 0){
-            throw new std::invalid_argument("Invalid data for name!");
+        if (name.empty()){
+            throw std::invalid_argument("Invalid data for name!");
         }
         this->name = name;
     }
 
     void setBirthYear(int year){
         if (year <= 1900 || year >= 2500){
-            throw new std::invalid_argument("You sure you born that year bro?");
+            throw std::invalid_argument("You sure you born that year bro?");
         }
 
         this->birthYear = year;
